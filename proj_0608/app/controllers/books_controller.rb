7beb3165be74334131c72ae62d53e1b3e08197f4
서원석 <younha1000@daum.net>
2020-06-14 16:41:16 +0900

    크롤링 기능추가, 책 추가시 유저 확인절차 추가

--- proj_0608/app/controllers/books_controller.rb
+++ proj_0608/app/controllers/books_controller.rb
@@ -1,41 +1,53 @@
 class BooksController < ApplicationController
     def new
-        
+
+    end
+
+
+    def search
+        if params[:ISBN].length == 13
+            if book = Book.find_by(ISBN: params[:ISBN])
+
+            else
+                url = "http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=" + params[:ISBN]
+                require 'open-uri'
+                
+                doc = Nokogiri::HTML(open(url))
+	  
+	            book = Book.new
+	            book.ISBN = params[:ISBN]
+	            page = doc.css('.content_left .box_detail_content .table_opened tr td')
+	            page = page[1].text
+	            page = page.delete "쪽"
+	            book.page = Integer(page)
+	            genre = doc.css('.list_detail_category li[1] a[1]')
+	            book.genre = genre[0].text
+	            book.title = doc.css('.box_detail_point h1.title').text
+	            book.author = doc.css('.box_detail_point .author .name .detail_author').text
+	            image = doc.css('.box_detail_info .box_detail_cover .cover').at("img[src]")
+	            book.imageURL = image['src']
+	            book.publisher = doc.css('.box_detail_point .author span[3] a').text
+	  
+	            book.save
+            end
+            @flag = 1
+        else
+            @flag = 2
+        end
     end
 
     def create
-        if params[:ISBN] == ''
+        book = Book.find_by(ISBN:params[:ISBN])
+        if Read.find_by(book_id:book.id, user_id:current_user.id)
             redirect_to '/'
         else
-            if  book = Book.find_by(ISBN: params[:ISBN])
-                if Read.find_by(book_id:book.id, user_id:current_user.id)
-                    redirect_to '/'
-                else
-                    read = Read.new
-                    read.user_id = current_user.id
-                    read.book_id = book.id
-                    read.status = false
-                    read.save
-                    redirect_to '/'
-                end
-            else
-                book = Book.new
-                book.ISBN = params[:ISBN]
-                book.page = params[:page]
-                book.genre = params[:genre]
-                book.title = params[:title]
-                book.author = params[:author]
-                book.publisher = params[:publisher]
-                book.save
-    
-                read = Read.new
-                read.user_id = current_user.id
-                read.book_id = book.id
-                read.status = false
-                read.save
-    
-                redirect_to '/'
-            end
+            read = Read.new
+            read.user_id = current_user.id
+            read.book_id = book.id
+            read.status = false
+            read.save
+            redirect_to '/'
         end
+        
     end
 end
